13df9330012a90b5ce346c141bb34b3eb2b214de
= <=>
2019-01-04 16:50:56 +0000

    test

---
+++ src/test/java/com/cucumber/AutoFrameRunner.java
@@ -0,0 +1,14 @@
+package com.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/")
+// the above path will run all the features within that features folder.
+public class AutoFrameRunner {
+		
+
+}
---
+++ target/test-classes/features/autoFrame.feature
@@ -0,0 +1,5 @@
+Feature: Testing different features
+
+Scenario: Check the API call returns 200
+When user makes an API call then they get a valid status code back 
+ 
